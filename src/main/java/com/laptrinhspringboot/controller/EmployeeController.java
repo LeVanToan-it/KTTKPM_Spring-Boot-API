853d684938b5439f4d8493097d0c7d3ad44c5e71
Le Van Toan <lvtoan.it2000@gmail.com>
2021-09-15 11:36:53 +0700

    create employee

--- src/main/java/com/laptrinhspringboot/controller/EmployeeController.java
+++ src/main/java/com/laptrinhspringboot/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.laptrinhspringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -9,5 +10,16 @@ import com.laptrinhspringboot.entity.Employees;
 import com.laptrinhspringboot.service.EmployeeService;
 
 public class EmployeeController {
+	@Autowired
+	private EmployeeService employeeService;
 	
+	@PostMapping(value="/createEmployee")
+	public Employees createEmployee(@RequestBody Employees employee) {
+		return employeeService.create(employee);
+	}
+	
+	@GetMapping(value="/findEmployee")
+	public Iterable<Employees> findEmployeeByID(@PathVariable("id")Long employeeID) {
+		return null;
+	}
 }
